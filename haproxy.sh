92c5b51fed3de33fc7db1d2c458ad1de6bab6dac
Jacobus Bothma <49413287+j-bothma@users.noreply.github.com>
2023-08-09 10:55:07 +0200

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -12,10 +12,10 @@ ufw disable
 
 wget -O /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/j-bothma/haproxy/main/haproxy.cfg
 
-mkdir /home/ubuntu/certs/
+mkdir /etc/certs
 
-wget -O /home/ubuntu/certs/cert.pem.rsa https://raw.githubusercontent.com/j-bothma/haproxy/main/cert.pem.rsa
+wget -O /etc/certs/cert.pem.rsa https://raw.githubusercontent.com/j-bothma/haproxy/main/cert.pem.rsa
 
-wget -O /home/ubuntu/certs/cert.pem.ecdsa https://raw.githubusercontent.com/j-bothma/haproxy/main/cert.pem.ecdsa
+wget -O /etc/certs/cert.pem.ecdsa https://raw.githubusercontent.com/j-bothma/haproxy/main/cert.pem.ecdsa
 
 systemctl start haproxy
